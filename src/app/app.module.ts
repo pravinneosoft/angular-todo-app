9631344547d61299e75a4f5de5a9f93eeb2bf7c2
pravin satpute <pravin96satpute@gmail.com>
2022-11-17 16:26:20 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './mycompenent/todos/todos.component';
+import { TodoItemsComponent } from './mycompenent/todo-items/todo-items.component';
+import { AddTodoComponent } from './mycompenent/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemsComponent,
+    AddTodoComponent
+   
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
